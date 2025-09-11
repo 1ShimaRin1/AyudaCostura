cc096bfbb409cb44b8ca6464f12bf9096314a582
ShimaRin <131628807+1ShimaRin1@users.noreply.github.com>
2025-09-11 15:46:14 -0300

    Primeros Activity

---
+++ app/src/main/java/com/example/ayudacostura/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.ayudacostura;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Espera 3 segundos y abre MainActivity
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // evita volver al splash
+        }, 3000);
+    }
+}
