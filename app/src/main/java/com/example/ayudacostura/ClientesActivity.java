ece69cde5d791dd91b6fa92daa7dd12235a6b085
ShimaRin <131628807+1ShimaRin1@users.noreply.github.com>
2025-09-26 13:35:02 -0300

    Clientes y pedidos

--- app/src/main/java/com/example/ayudacostura/ClientesActivity.java
+++ app/src/main/java/com/example/ayudacostura/ClientesActivity.java
@@ -1,6 +1,10 @@
 package com.example.ayudacostura;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +13,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ClientesActivity extends AppCompatActivity {
+    private Button btnAgregarClientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_clientes);
+
+        btnAgregarClientes = findViewById(R.id.btnAgregarCliente);
+
+        btnAgregarClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ClientesActivity.this, AgregarCliente.class);
+                Toast.makeText(ClientesActivity.this, "Iniciando Crear Cliente", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
     }
 }
